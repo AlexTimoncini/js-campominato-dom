95a480690a2508f5addd718f45bbdc4c8ed79f5e
AlexTimoncini <alextimonci@gmail.com>
2023-05-24 16:14:54 +0200

    created popup with a function

--- js/script.js
+++ js/script.js
@@ -4,27 +4,23 @@
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-let windowDom = document.getElementById('document_wrapper')
+let windowDom = document.getElementById('document_wrapper');
 let gridParent = document.getElementById('grid');
 let gridGeneratorDom = document.getElementById('gridGenerator');
 let gridSelect = document.getElementById('grid-select');
-let popup = document.querySelector('.pop-up');
-let playAgainDom = document.getElementById('playAgain');
 
 gridGeneratorDom.addEventListener('click', function(){
     gridParent.innerHTML = '';
-    generateGame(gridSelect.value, gridParent, 16, popup, windowDom);
+    generateGame(gridSelect.value, gridParent, 16, windowDom);
 });
 
-playAgainDom.addEventListener('click', function(){
-    gridParent.innerHTML = '';
-    popup.style.transform = 'translateX(-50%) translateY(-200%)';
-    document.querySelector('.screen-wrapper').remove();
-    generateGame(gridSelect.value, gridParent, 16, popup, windowDom);
-});
-
-function generateGame(cellNumber, gridContainer, bombsNumber, popupDom, documentWindow){
+function generateGame(cellNumber, gridContainer, bombsNumber, documentWindow){
     let bombCells = randomIntArray(1, cellNumber, bombsNumber);
+    let flowerCells = 0;
+    let screenDarkDom = document.createElement('div');
+    screenDarkDom.classList.add('screen-wrapper');
+    createPopup(documentWindow);
+    let gameResult = document.getElementById('gameResult');
     for (let i = 0; i < cellNumber; i++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
@@ -32,21 +28,50 @@ function generateGame(cellNumber, gridContainer, bombsNumber, popupDom, document
         if (bombCells.includes(i + 1)){
             cell.addEventListener('click', function(){
                 this.classList.add('bomb');
-                let screenDarkDom = document.createElement('div');
-                screenDarkDom.classList.add('screen-wrapper');
                 documentWindow.appendChild(screenDarkDom);
-                popupDom.style.transform = 'translateX(-50%) translateY(-50%)';
+                document.querySelector('.pop-up').style.transform = 'translateX(-50%) translateY(-50%)';
+                gameResult.innerHTML = 'Game Over!'
             });
         } else {
-            cell.addEventListener('click', function(){
-                this.classList.add('flower');
+            cell.addEventListener('click', function flower(){
+                cell.classList.add('flower');
                 console.log('you clicked on ' + (i + 1));
+                if (flowerCells === (cellNumber - bombsNumber - 1)){
+                    documentWindow.appendChild(screenDarkDom);
+                    document.querySelector('.pop-up').style.transform = 'translateX(-50%) translateY(-50%)';
+                    gameResult.innerHTML = 'You Won!'
+                } else {
+                    flowerCells++;
+                }
             });
         }
         cell.style.width = 'calc(100% /' + Math.sqrt(cellNumber) + ')';
         gridContainer.append(cell);
     }
 };
+
+function createPopup(parent){
+    let popup = document.createElement('div');
+    popup.classList.add('pop-up');
+    parent.appendChild(popup);
+    popup.innerHTML = `
+        <h1 id="gameResult"></h1>
+        <button id="closeBtn">x</button>
+        <button id="playAgain">Gioca di nuovo!</button>
+    `;
+    let closeBtn = document.getElementById('closeBtn');
+    closeBtn.addEventListener('click', function(){
+        popup.style.transform = 'translateX(-50%) translateY(-200%)';
+        document.querySelector('.screen-wrapper').remove();
+    });
+    let playAgainBtn = document.getElementById('playAgain');
+    playAgainBtn.addEventListener('click', function(){
+        gridParent.innerHTML = '';
+        popup.style.transform = 'translateX(-50%) translateY(-200%)';
+        document.querySelector('.screen-wrapper').remove();
+        generateGame(gridSelect.value, gridParent, 16, windowDom);
+    });
+}
 
 function randomIntArray(min, max, times){
     if ((max - min) < times){
